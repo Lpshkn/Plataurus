d23b500e251a3566706fea5e250a0814cce5d87d
Lpshkn <lepkirill@yandex.ru>
2021-09-10 14:11:43 +0300

    Update 'index' route: add handling 'POST' and 'GET' requests

--- app/routes.py
+++ app/routes.py
@@ -1,8 +1,10 @@
 from app import app
-from flask import render_template
+from flask import render_template, request, jsonify
 
 
-@app.route('/')
-@app.route('/index')
+@app.route('/', methods=['POST', 'GET'])
 def index():
+    if request.method == 'POST':
+        return jsonify({"firstCommon": [[1, 3]],
+                        "secondCommon": [[2, 4]]})
     return render_template('index.html')
