b379e67ce9b6fbe22265380e6651b9854e36ba28
Lpshkn <lepkirill@yandex.ru>
2021-09-12 19:08:04 +0300

    Add getting 'hostname' and 'port' values from the environment to run a server with these parameters

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
 from app import app
 
+from plataurus.utils.variables import get_env_value
+from app.constants import FLASK_HOSTNAME, FLASK_PORT
+
 
 if __name__ == '__main__':
-    app.run()
+    hostname = get_env_value('FLASK_HOSTNAME', FLASK_HOSTNAME)
+    port = get_env_value('FLASK_PORT', FLASK_PORT)
+
+    app.run(host=hostname, port=port)
