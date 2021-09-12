07c6ec46a3053bd7f586de9fc6f8325913260692
Lpshkn <lepkirill@yandex.ru>
2021-09-12 18:36:46 +0300

    Add using 'get_clear_matches' function to the '/' route on 'POST' query

--- app/routes.py
+++ app/routes.py
@@ -1,10 +1,16 @@
+import json
+
 from app import app
 from flask import render_template, request, jsonify
+
+from plataurus.match import get_clear_matches
 
 
 @app.route('/', methods=['POST', 'GET'])
 def index():
     if request.method == 'POST':
-        return jsonify({"firstCommon": [[1, 3]],
-                        "secondCommon": [[2, 4]]})
+        data = json.loads(request.data)
+        first_matches, second_matches = get_clear_matches(data['firstText'], data['secondText'])
+        return jsonify({"firstCommon": first_matches,
+                        "secondCommon": second_matches})
     return render_template('index.html')
