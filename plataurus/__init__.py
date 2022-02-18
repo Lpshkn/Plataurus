ab2e26b6b5a35e9937894f5ba5e833ed1e340379
Lpshkn <lepkirill@yandex.ru>
2022-02-18 13:40:37 +0300

    Add the name of the package into __all__

--- plataurus/__init__.py
+++ plataurus/__init__.py
@@ -2,12 +2,13 @@ __package_name__ = "Plataurus"
 __version__ = "0.1.0"
 
 __author__ = "Kirill Lepeshkin"
-__email__ = 'lepkirill@yandex.ru'
+__email__ = "lepkirill@yandex.ru"
 __maintainer__ = __author__
 
 __all__ = (
-    '__author__',
-    '__email__',
-    '__maintainer__',
-    '__version__',
+    "__author__",
+    "__email__",
+    "__maintainer__",
+    "__version__",
+    "__package_name__"
 )
