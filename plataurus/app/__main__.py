2c313c6d7a53cf85bf64ef26298cf03d17af3457
Lpshkn <lepkirill@yandex.ru>
2022-02-18 14:17:01 +0300

    Move the package inside to plataurus package

--- plataurus/app/__main__.py
+++ plataurus/app/__main__.py
@@ -1,7 +1,6 @@
-from app import app
-
+from plataurus.app import app
 from plataurus.utils.variables import get_env_value
-from app.constants import FLASK_HOSTNAME, FLASK_PORT
+from plataurus.app.constants import FLASK_HOSTNAME, FLASK_PORT
 
 
 if __name__ == '__main__':
