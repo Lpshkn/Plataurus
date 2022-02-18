100364ed413874aa1c416be5f7be47b92a34239e
Lpshkn <lepkirill@yandex.ru>
2022-02-18 14:19:43 +0300

    Fix imports after reorganizing of the project

--- plataurus/app/routes.py
+++ plataurus/app/routes.py
@@ -1,10 +1,10 @@
 import json
 
-from app import app
 from flask import render_template, request, jsonify
 
+from plataurus.app import app
 from plataurus.match import get_matches
-from app.constants import FIRST_TEXT, SECOND_TEXT
+from plataurus.app.constants import FIRST_TEXT, SECOND_TEXT
 
 
 @app.route('/', methods=['POST', 'GET'])
@@ -12,6 +12,8 @@ def index():
     if request.method == 'POST':
         data = json.loads(request.data)
         first_matches, second_matches = get_matches(data['firstText'], data['secondText'], threshold=data['rangeValue'])
+
         return jsonify({"firstCommon": first_matches,
                         "secondCommon": second_matches})
+
     return render_template('index.html', first_text=FIRST_TEXT, second_text=SECOND_TEXT)
