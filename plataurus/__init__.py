e0e276f95ac76ca6350cab4da897ad6310e102d7
Lpshkn <lepkirill@yandex.ru>
2022-02-18 13:37:30 +0300

    Update information about the package

---
+++ plataurus/__init__.py
@@ -0,0 +1,12 @@
+__version__ = "0.1.0"
+
+__author__ = "Kirill Lepeshkin"
+__email__ = 'lepkirill@yandex.ru'
+__maintainer__ = __author__
+
+__all__ = (
+    '__author__',
+    '__email__',
+    '__maintainer__',
+    '__version__',
+)
