d449b740e1a6fd4285a9a0f20635a5b88505576c
Lpshkn <lepkirill@yandex.ru>
2022-02-20 22:18:52 +0300

    Add getting of relative path of that module

--- plataurus/api/app.py
+++ plataurus/api/app.py
@@ -1,6 +1,7 @@
 import jinja2
 import aiohttp_jinja2
 from aiohttp import web
+from os.path import dirname, join
 
 from plataurus.api.views.main_page import MainPage
 
@@ -8,7 +9,7 @@ from plataurus.api.views.main_page import MainPage
 def create_app() -> web.Application:
     app_main = web.Application()
 
-    aiohttp_jinja2.setup(app_main, loader=jinja2.FileSystemLoader("templates"))
+    aiohttp_jinja2.setup(app_main, loader=jinja2.FileSystemLoader(join(dirname(__file__), "templates")))
 
     app_main.router.add_view("/", MainPage)
 
